188b0a53f3b6126b659baa50654e566700cb920c
Hazim Emam <84563544+HazimEmam@users.noreply.github.com>
2024-10-20 18:03:19 +0300

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Step 1: Build the Go backend
-echo "Building Go backend..."
-docker build -t hazimemam/go-app:1.0 ./Clinic-Reservation-System
+# # Step 1: Build the Go backend
+# echo "Building Go backend..."
+# docker build -t hazimemam/go-app:1.0 ./Clinic-Reservation-System
 
-# Step 2: Build the React frontend
-echo "Building React frontend..."
-docker build -t hazimemam/react-app:1.0 ./front_end_project
+# # Step 2: Build the React frontend
+# echo "Building React frontend..."
+# docker build -t hazimemam/react-app:1.0 ./front_end_project
 
 
-# Step 3: Push Containers
-echo "Pushing the continer..."
-docker login
-docker push hazimemam/go-app:1.0
-docker push hazimemam/react-app:1.0
+# # Step 3: Push Containers
+# echo "Pushing the continer..."
+# docker login
+# docker push hazimemam/go-app:1.0
+# docker push hazimemam/react-app:1.0
 
 # Step 4: Start Compose
 echo "Starting services with Docker Compose..."
